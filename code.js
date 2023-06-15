d888faa3373df6ff18c17dc41fe15096376847d1
Gabriel Lawson <gabriel@Gabriels-MacBook-Pro.local>
2023-06-15 09:34:31 -0700

    Fixed form Grid layout and hover

--- code.js
+++ code.js
@@ -1,15 +1,15 @@
 // The goal here is to have a little dialog box pop-up when the client first lands on the main page.
 
-const name = `Gabe`;
-const hello = `welcome to my personal website! Enjoy your stay!`;
-const greet = `Hi, this is ${name}!, ${hello}`;
-console.log(greet);
-alert(greet);
+// const name = `Gabe`;
+// const hello = `welcome to my personal website! Enjoy your stay!`;
+// const greet = `Hi, this is ${name}!, ${hello}`;
+// console.log(greet);
+// alert(greet);
 
-const form = document.querySelector("#form")
-   const submitButton = document.querySelector("#submit")
+const form = document.querySelector("#sub-form")
+   const submitButton = document.querySelector("#sub-button")
    const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
-
+console.log(form)
    form.addEventListener('submit', e => {
      submitButton.disabled = true
      e.preventDefault()
