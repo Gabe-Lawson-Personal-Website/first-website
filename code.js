f129f10d513c8154e0b0070f31c9318ff29efb16
Gabriel Lawson <gabriel@Gabriels-MacBook-Pro.local>
2023-06-20 15:37:24 -0700

    temporarely disabled js form code

--- code.js
+++ code.js
@@ -6,23 +6,23 @@
 // console.log(greet);
 // alert(greet);
 
-const form = document.querySelector("#sub-form")
-   const submitButton = document.querySelector("#sub-button")
-   const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
-console.log(form)
-   form.addEventListener('submit', e => {
-     submitButton.disabled = true
-     e.preventDefault()
-     let requestBody = new FormData(form)
-     fetch(scriptURL, { method: 'POST', body: requestBody})
-       .then(response => {
-          alert('Success!', response)
-          submitButton.disabled = false
-         })
-       .catch(error => {
-       alert('Error!', error.message)
-         submitButton.disabled = false
+// const form = document.querySelector("#sub-form")
+//    const submitButton = document.querySelector("#sub-button")
+//    const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+// console.log(form)
+//    form.addEventListener('submit', e => {
+//      submitButton.disabled = true
+//      e.preventDefault()
+//      let requestBody = new FormData(form)
+//      fetch(scriptURL, { method: 'POST', body: requestBody})
+//        .then(response => {
+//           alert('Success!', response)
+//           submitButton.disabled = false
+//          })
+//        .catch(error => {
+//        alert('Error!', error.message)
+//          submitButton.disabled = false
 
-       }
-       )
-   })
+//        }
+//        )
+//    })
